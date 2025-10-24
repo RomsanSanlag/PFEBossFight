daaf416c0c984fee6aff4790437e53b8f0f7c13e
Selsanat <leandro.benites@edu.devinci.fr>
2025-10-24 11:43:56 +0200

    [ ADD ] : Slow time on dodge

---
+++ Source/BossFight/private/Character/Features/PersistingDodgeHitbox.cpp
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Character/Features/PersistingDodgeHitbox.h"
+
+
+// Sets default values
+APersistingDodgeHitbox::APersistingDodgeHitbox()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void APersistingDodgeHitbox::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void APersistingDodgeHitbox::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
