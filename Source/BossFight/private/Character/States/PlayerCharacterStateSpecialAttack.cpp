d93a04db6c532a286ced49ceaa7e04b4cc8727c6
Léandro Benites <l.benites@rivrs.io>
2025-12-16 13:01:46 +0100

    [ ADD ] : Player Animations

--- Source/BossFight/private/Character/States/PlayerCharacterStateSpecialAttack.cpp
+++ Source/BossFight/private/Character/States/PlayerCharacterStateSpecialAttack.cpp
@@ -499,7 +499,8 @@ void UPlayerCharacterStateSpecialAttack::SpawnShootVFX()
 						
 						NiagaraComp->Activate(true);
 					}
-
+					// === APPEL DE OnLaserLaunched AVEC L'ACTEUR HITTÉ ===
+					Character->OnLaserLaunched(HitActor);
 					// --- DELAY AVANT LE FREEZE ---
 					FTimerHandle DelayBeforeFreezeHandle;
 					GetWorld()->GetTimerManager().SetTimer(
@@ -525,8 +526,6 @@ void UPlayerCharacterStateSpecialAttack::SpawnShootVFX()
 						0.2f, // <= TON DELAY
 						false
 					);
-					// === APPEL DE OnLaserLaunched AVEC L'ACTEUR HITTÉ ===
-					Character->OnLaserLaunched(HitActor);
 				}
 			}
 		}
